c8d1dc331614a25fd9f8e64c2cf420689996d333
onestyking <francesco.colanera@gmail.com>
2025-05-23 18:48:33 +0200

    Update scrivi_hello.py

--- scrivi_hello.py
+++ scrivi_hello.py
@@ -1,4 +1,5 @@
 
-with open("output.txt", "w") as f:
-    f.write("Hello,World!")
+from datetime import datetime
 
+with open("output.txt", "w") as f:
+    f.write(f"Hello, World! - aggiornato alle {datetime.utcnow()} UTC\n")
