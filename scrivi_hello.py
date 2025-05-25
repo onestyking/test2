cfe83e12d8f6258558ea0c52555a6b686979c947
onestyking <francesco.colanera@gmail.com>
2025-05-25 18:53:20 +0200

    Update scrivi_hello.py

--- scrivi_hello.py
+++ scrivi_hello.py
@@ -3,5 +3,5 @@ import telegram
 TOKEN = "TUO_TOKEN_BOT"
 CHAT_ID = "TUO_CHAT_ID"
 
-bot = telegram.Bot(token=TOKEN)
-bot.send_message(chat_id=CHAT_ID, text="Ciao!")
+bot = telegram.Bot(token='6031474825:AAHycNIt1QIk5jNbUcdwj5fYUvwgW147Gu4')
+bot.send_message(chat_id='734074925', text="Ciao!")
